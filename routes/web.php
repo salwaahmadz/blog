120b165412b6a298b5b6c24774982525d3c95b92
salwaahmadz <salwaahmadz888@gmail.com>
2019-01-28 11:05:13 +0700

    Reset to Default

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
 //Route::get('/home', 'HomeController@index')->name('home');
