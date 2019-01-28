d0af603766d1fb57c05d3871f13405bed456d8f6
salwaahmadz <salwaahmadz888@gmail.com>
2019-01-28 10:34:30 +0700

    develop

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Auth::routes();
 
 //Route::get('/home', 'HomeController@index')->name('home');
